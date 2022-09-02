395577232e656085e8841e026ba49b5529858241
Linus J <linus.jansson2000@gmail.com>
2022-09-02 12:24:26 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>
-          Sida gjord av Linus J
-        </h3>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Skapad med create react app
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { isHovered: false };
+    // bind toggleHover function to this
+    this.toggleHover = this.toggleHover.bind(this);
+  }
+  
+  toggleHover() {
+    // change hover to !hover
+    this.setState(prevState => ({isHovered: !prevState.isHovered}));
+  }
+
+  render()
+  {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h3>Made By Linus Jansson</h3>
+          <p>Want to get in contact with me?</p>
+          <div onMouseOver={() => this.setState({isHovered: true})} onMouseOut={() => this.setState({isHovered: false}) } >
+            { this.state.isHovered ?
+                <a className="App-link" 
+                href="mailto:contact@limpan.dev" 
+                target="_blank" 
+                rel="noopener noreferrer">
+                    contact@limpan.dev
+                </a> :
+                <a className="App-link" 
+                    href="mailto:contact@limpan.dev" 
+                    target="_blank" 
+                    rel="noopener noreferrer">
+                    Mail me!
+                </a>
+            }
+            
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('Checks for made by linus text on pasge', () => {
   render(<App />);
-  const linkElement = screen.getByText(/sida gjord av Linus J/i);
+  const linkElement = screen.getByText(/Made By Linus J/i);
   expect(linkElement).toBeInTheDocument();
 });
