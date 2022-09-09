f459a6387c74fd26a6309fa32d1938834260f1eb
linus-jansson <contact@limpan.dev>
2022-09-09 22:01:24 +0200

    testing cards for 'my work'

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
+
 import styles from '../styles/Home.module.css' // not using
 
+import Card from '../components/Card.jsx'
+
 export default function Home() {
+    const cards = []
+    let desc_data = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, se"
+    for (let i = 0; i < 12; i++) {
+
+        cards.push(<Card title={"test"} id={`card-${i}`} description={desc_data} href="https://example.com/"  />)
+    }
+
     return (
     <div className={styles.container}>
         <Head>
@@ -13,13 +23,8 @@ export default function Home() {
             <h1>header</h1>
         </header>
         <main>
-            <div id='my-work' className='container'>
-                <h1>LINUS</h1>
-                <div className='item'>LINUS</div>
-                <div className='item'>LINUS</div>
-                <div className='item'>LINUS</div>
-                <div className='item'>LINUS</div>
-                <div className='item'>LINUS</div>
+            <div id='my-work' className='cards'>
+                {cards}
             </div>
             <div id='skills' className='container'>
                 <p>test</p>
