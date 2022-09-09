5498d49898d79c5304bea64d3b551ac59f5cfa21
Linus J <linus.jansson2000@gmail.com>
2022-09-09 14:32:48 +0200

    initial nextjs install

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('Checks for made by linus text on pasge', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Made By Linus J/i);
-  expect(linkElement).toBeInTheDocument();
-});
