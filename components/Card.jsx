ca2f146786f0707d649679c2671a71694725c3bb
Linus J <linus.jansson2000@gmail.com>
2022-09-16 15:46:07 +0200

    target_blank

--- components/Card.jsx
+++ components/Card.jsx
@@ -6,13 +6,15 @@ import styles from '../styles/Card.module.css'
 export default function Card( props ) {
     return (
         <Link href={props.href}>
-            <div className={styles.card} key={props.id}>
-                <div className={styles.card_image}></div>
-                <div className={styles.card_content}>
-                    <h3>{props.title}</h3>
-                    <p>{props.description}</p>
+            <a target='_blank'>
+                <div className={styles.card} key={props.id}>
+                    <div className={styles.card_image}></div>
+                    <div className={styles.card_content}>
+                        <h3>{props.title}</h3>
+                        <p>{props.description}</p>
+                    </div>
                 </div>
-            </div>
+            </a>
         </Link>
         
     )
