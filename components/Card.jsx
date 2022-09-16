6303b28741886c3f74686574ce4f1b5cd11afef7
Linus J <linus.jansson2000@gmail.com>
2022-09-16 14:31:43 +0200

    made the card as a link

--- components/Card.jsx
+++ components/Card.jsx
@@ -5,13 +5,15 @@ import styles from '../styles/Card.module.css'
 
 export default function Card( props ) {
     return (
-        <div className={styles.card} key={props.id}>
-            <div className={styles.card_image}></div>
-            <div className={styles.card_content}>
-                <h3>{props.title}</h3>
-                <p>{props.description}</p>
-
+        <Link href={props.href}>
+            <div className={styles.card} key={props.id}>
+                <div className={styles.card_image}></div>
+                <div className={styles.card_content}>
+                    <h3>{props.title}</h3>
+                    <p>{props.description}</p>
+                </div>
             </div>
-        </div>
+        </Link>
+        
     )
 }
