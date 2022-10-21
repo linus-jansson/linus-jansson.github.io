264a6ed19744a99aada6fe54739e0b1e6cb5862d
linus-jansson <30862662+linus-jansson@users.noreply.github.com>
2022-10-21 22:15:22 +0000

    Deploying to gh-pages from @ linus-jansson/linus-jansson.github.io@758bb0d36333a2cb0bcfad081e3f3c27a5aad743 🚀

--- _next/static/McochwyV90qvH0tG6K9fz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/986-efa7c93dfe58dffe.js","static/css/6e67cb5cd4b3d798.css","static/chunks/pages/index-bc4357a34c3b20a8.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
