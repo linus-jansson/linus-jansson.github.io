1be6867101cf0eb6b950a26bb3f221c22fbf2c92
linus-jansson <30862662+linus-jansson@users.noreply.github.com>
2022-10-21 08:14:04 +0000

    Deploying to gh-pages from @ linus-jansson/linus-jansson.github.io@2f4435de23657b91a24af04a5611ed8175397dd5 🚀

---
+++ _next/static/2s3drxry3sUPTexCxDmd3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-e4875211653c5b62.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
