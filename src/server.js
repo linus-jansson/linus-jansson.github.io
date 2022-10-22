2be4a18d57504d920a7945d020b32324c71f9f4f
linus-jansson <contact@limpan.dev>
2022-10-22 20:50:22 +0200

    restructuring

--- src/server.js
+++ src/server.js
@@ -12,6 +12,8 @@ const dev = process.env.NODE_ENV !== 'production'
 const nextApp = next({ dev })
 const nextHandler = nextApp.getRequestHandler()
 
+import Auth from './auth.js';
+
 nextApp.prepare().then(() => {
 
     /* Set up body-parser */
@@ -19,9 +21,18 @@ nextApp.prepare().then(() => {
         extended: true
     }));
 
+    server.get('/login', (req, res) => {
+        let auth = new Auth();
+        auth.login((result) => {
+            console.log(result)
+            return res.json(auth.authenticated)
+        });
+
+    });
+
     /* Handle all requests through next */
     server.get("*", (req, res) => {
-        return nextHandler(req, res)
+        return nextHandler(req, res);
     });
 
     /* Listen on port 8000 */
