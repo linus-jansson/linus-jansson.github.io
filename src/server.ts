868f6f1d41c7b3d3b167a33f66be75b79f4d8b1d
linus-jansson <contact@limpan.dev>
2022-10-25 19:31:50 +0200

    Code formating and correct import of http

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import type { Request, Response } from 'express';
 import next from 'next'
 import bodyParser from 'body-parser';
-import http from 'http';
+import * as http from 'http';
 
 const server = express()
 const http_server = new http.Server(server);
@@ -15,33 +15,36 @@ const nextHandler = nextApp.getRequestHandler()
 
 // import Auth from './auth';
 
-nextApp.prepare().then((): void => {
+nextApp
+    .prepare()
+    .then((): void => {
 
-    /* Set up body-parser */
-    server.use(bodyParser.urlencoded({
-        extended: true
-    }));
+        /* Set up body-parser */
+        server.use(bodyParser.urlencoded({
+            extended: true
+        }));
 
-    // server.get('/login', (req, res) => {
-    //     let auth = new Auth();
-    //     auth.login((result) => {
-    //         console.log(result)
-    //         return res.json(auth.isAuthenticated())
-    //     });
+        // server.get('/login', (req, res) => {
+        //     let auth = new Auth();
+        //     auth.login((result) => {
+        //         console.log(result)
+        //         return res.json(auth.isAuthenticated())
+        //     });
 
-    // });
+        // });
 
-    /* Handle all requests through next */
-    server.get("*", (req: Request, res: Response) => {
-        return nextHandler(req, res);
-    });
+        /* Handle all requests through next */
+        server.get("*", (req: Request, res: Response) => {
+            return nextHandler(req, res);
+        });
 
-    /* Listen on port 8000 */
-    // https://stackoverflow.com/questions/56291321/how-to-handle-errors-with-express-listen-in-typescript
-    const PORT: number = 8000;
-    http_server.listen(PORT, (): void => {
-        console.log("Server is running on port 8000")
-    }).on('error', (err: Error) => {
-        throw err
-    });
-});
+        /* Listen on port 8000 */
+        // https://stackoverflow.com/questions/56291321/how-to-handle-errors-with-express-listen-in-typescript
+        const PORT: number = 8000;
+        http_server.listen(PORT, (): void => {
+            console.log("Server is running on port 8000")
+        }).on('error', (err: Error) => {
+            throw err
+        });
+    })
+    .catch((err) => { console.log(err) });
