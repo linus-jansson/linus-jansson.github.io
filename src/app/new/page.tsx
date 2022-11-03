b953ec9becfd0c4f3cd20ceb1e86610823a3fe2e
linus-jansson <contact@limpan.dev>
2022-11-03 17:38:43 +0100

    some scaling

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -3,22 +3,22 @@ import { FaGithub, FaLink, FaLinkedin, FaDiscord, FaEnvelope } from 'react-icons
 
 const PortfolioCard = () => {
     return (
-        <Link href='https://example.com'>
-            <div className='bg-zinc-800 w-full p-4 rounded-lg md:hover:scale-125 duration-300 my-4'>
+        <div className='bg-zinc-800 w-full transition rounded-lg md:hover:scale-125 md:hover:opacity-100 duration-300 my-4'>
+            <Link href='https://example.com'>
                 <p className='text-slate-100 uppercase'>Project Type</p>
                 <p className='text-slate-100'>Project Name</p>
                 <p className='text-slate-100'>Project Description</p>
                 <p className='text-slate-100'>Project Description</p>
 
-            </div>
-        </Link>
+            </Link>
+        </div>
     )
 }
 
 const Socials = (props: any) => {
     return (
         <>
-            <Link href={props.link} target='_blank' className='text-slate-100 md:text-sm hover:text-slate-300 duration-100 pr-4 '>
+            <Link href={props.link} target='_blank' className='text-slate-100 md:text-sm hover:text-slate-300 duration-100 pr-4'>
                 <span className='mr-2 inline-block align-middle' >
                     <props.icon />
                 </span>
@@ -43,7 +43,7 @@ export default function IndexPage() {
     return (
         <>
             <div className='flex flex-col md:flex-row pt-4 md:relative'>
-                <div className='mx-auto md:pl-24 text-left md:h-screen md:flex md:flex-col md:justify-around md:fixed'>
+                <div className='w-full mx-auto md:pl-24 text-left md:h-screen md:flex md:flex-col md:justify-around md:fixed'>
 
                     <div id='about'>
                         <p className='text-slate-100 text-3xl font-bold'>Hello I'm Linus</p>
@@ -59,7 +59,7 @@ export default function IndexPage() {
                         <Socials link="mailto:contact@limpan.dev" icon={FaEnvelope} icontext='Mail Me' />
                     </div>
                 </div>
-                <div className='md:absolute md:right-0 md:pr-10 mb-12 w-1/2 md:max-w-[536px] lg:max-w-[736px] flex flex-col'>
+                <div className='mx-auto md:absolute md:right-0 md:pr-24 md:pl-48 mb-12 w-full md:max-w-[536px] lg:max-w-[736px] flex flex-col'>
                     <PortfolioCard />
                     <PortfolioCard />
                     <PortfolioCard />
