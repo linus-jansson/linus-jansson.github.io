0c97a419cc18c612ecedd01715474fec857fb3e8
linus-jansson <contact@limpan.dev>
2022-11-27 16:32:37 +0100

    overflow x hidden for hover scale on large screens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className='bg-zinc-900'>
+            <body className='bg-zinc-900 overflow-x-hidden'>
                 {children}
             </body>
         </html>
