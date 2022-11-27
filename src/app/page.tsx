5d629e35bc79cfd9968269c1a8a4da4a2bf59fd3
linus-jansson <contact@limpan.dev>
2022-11-27 19:19:16 +0100

    seperate data file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import styles from './timeline.module.css'
 import { FaGithub, FaLink } from 'react-icons/fa';
 import Header from './Header'
+import Data from './data'
 
 const PortfolioCard = ({ type, title, desc, github, page_link }:
     {
@@ -25,6 +26,30 @@ const PortfolioCard = ({ type, title, desc, github, page_link }:
     )
 }
 
+const PortfolioSection = () => {
+    return (
+        <>
+            <h1 className='text-slate-100 text-3xl font-bold uppercase tracking-widest text-center'>My Recent Projects</h1>
+            <div id='projects'>
+                {Data.projects.map((project, index) => {
+                    return (
+                        <PortfolioCard
+                            key={index}
+                            title={project.title}
+                            type={project.type}
+                            desc={project.desc}
+                            github={project?.github}
+                            page_link={project?.page_link}
+                        />
+                    )
+                })}
+
+
+            </div>
+        </>
+    )
+}
+
 const TimelineCard = ({ title, children }: { title: string, children: React.ReactNode }) => {
     return (
         <div className={styles.card}>
@@ -36,95 +61,47 @@ const TimelineCard = ({ title, children }: { title: string, children: React.Reac
     )
 }
 
-const Data = {
-    projects: [
-        {
-            type: 'Full stack',
-            title: 'Adminnärvaro',
-            desc: 'A fulllstack web application for seeing the attendence of the personeel at NTI gymnasiet. Built with Next.js, JWT, Socket.io, Express.js, tailwindcss and Mongodb. The application also has an API that is used by a raspberry pico W to update the attendence for a person with a button press.',
-            page_link: 'https://narvaro.ntig.net/'
-        },
-        {
-            type: 'Full stack (ongoing)',
-            title: 'Website builder',
-            desc: 'A full stack application for creating websites which can be exported as a HTML file. Built with Next.js and Tailwindcss.',
-            github: 'https://github.com/linus-jansson/webpage-builder'
-        },
-        {
-            type: 'C# (ongoing)',
-            title: 'POS system',
-            desc: 'A POS system for hotels. Built with c# and winforms. Also uses NUnit Tests for unit testing.',
-            github: 'https://github.com/NTIG-Uppsala/hotell-kassasystem'
-        },
-        {
-            type: 'Frontend',
-            title: 'Pizzeria Rafiki',
-            desc: 'A website for a pizzeria. Has automated tests using Github workflos and selenium. Deployed using Github pages',
-            github: 'https://github.com/NTIG-Uppsala/pizzeria-rafiki/',
-            page_link: 'https://ntig-uppsala.github.io/pizzeria-rafiki/'
-        },
-        {
-            type: 'Frontend',
-            title: 'Frisör Saxé',
-            desc: 'A website for a hairdresser. Uses selenium for automated testing in combination with Github workflows.',
-            github: 'https://github.com/NTIG-Uppsala/Frisor-Saxe/',
-            page_link: 'https://ntig-uppsala.github.io/Frisor-Saxe/'
-        },
-        {
-            type: 'Frontend',
-            title: 'Florist Blåklinten',
-            desc: 'A website for a flowershop. Uses selenium SDK for unit tests and is deployed using Github Workflows on Github Pages',
-            github: 'https://github.com/NTIG-Uppsala/florist-blaklinten/',
-            page_link: 'https://ntig-uppsala.github.io/florist-blaklinten/'
-        },
-    ],
-    timeline: [{}]
+const TimelineSection = () => {
+    return (
+        <>
+            <div id='timeline' className='p-12'>
+                <h1 className='text-slate-100 text-3xl font-bold uppercase text-center tracking-widest'>My Timeline</h1>
+                <div>
+                    <TimelineCard title="Teknikprogrammet (TE)">
+                        <p>I went to NTI-gymnasiet, a high school which has it's main focus in tech where from I graduated in June 2022.</p>
+                    </TimelineCard>
+                    <TimelineCard title="Gymnasieingenjör (T4)">
+                        <p>I am currently studying to become a <i>"Gymnasieingenjör"</i> at NTI-gymnasiet, an additional year to high shool which prepares me to work in software development.</p>
+                    </TimelineCard>
+                </div>
+                <p className='text-center text-zinc-600 font-thin'>Want to Hire me? <a href='mailto:contact@limpan.dev' className='underline'>Hit me up!</a></p>
+            </div>
+        </>
+    )
 }
 
 export default function IndexPage() {
 
     return (
         <>
+            {/* Mobile Navbar */}
             <div className='md:hidden z-50 sticky shadow-2xl w-screen top-0 bg-rose-700 p-4 flex flex-row justify-around xs:flex-col'>
                 <a href="#start" className='text-slate-100 hover:text-slate-300 duration-100 font-bold uppercase tracking-widest hover:underline'>About Me</a>
                 <a href="#projects" className='text-slate-100 hover:text-slate-400 duration-100 font-bold uppercase tracking-widest hover:underline'>Projects</a>
                 <a href="#timeline" className='text-slate-100 hover:text-slate-400 duration-100 font-bold uppercase tracking-widest hover:underline'>Timeline</a>
             </div>
             <div id='start' className='scroll-mt-12 flex flex-col lg:flex-row pt-4'>
+                {/* Sidebar with links and about text */}
                 <Header />
                 <div className='px-4 md:ml-auto md:pr-24 md:pl-48 md:pt-24 mb-12 w-full md:w-1/2 flex flex-col'>
-                    <h1 className='text-slate-100 text-3xl font-bold uppercase tracking-widest text-center'>My Recent Projects</h1>
-                    <div id='projects'>
-                        {Data.projects.map((project, index) => {
-                            return (
-                                <PortfolioCard
-                                    key={index}
-                                    title={project.title}
-                                    type={project.type}
-                                    desc={project.desc}
-                                    github={project?.github}
-                                    page_link={project?.page_link}
-                                />
-                            )
-                        })}
-
-
-                    </div>
-                    <div id='timeline' className='p-12'>
-                        <h1 className='text-slate-100 text-3xl font-bold uppercase text-center tracking-widest'>My Timeline</h1>
-                        <div>
-                            <TimelineCard title="Teknikprogrammet (TE)">
-                                <p>I went to NTI-gymnasiet, a high school which has it's main focus in tech where from I graduated in June 2022.</p>
-                            </TimelineCard>
-                            <TimelineCard title="Gymnasieingenjör (T4)">
-                                <p>I am currently studying to become a <i>"Gymnasieingenjör"</i> at NTI-gymnasiet, an additional year to high shool which prepares me to work in software development.</p>
-                            </TimelineCard>
-                        </div>
-                        <p className='text-center text-zinc-600 font-thin'>Want to Hire me? <a href='mailto:contact@limpan.dev' className='underline'>Hit me up!</a></p>
-                    </div>
+                    {/* Projects section */}
+                    <PortfolioSection />
+                    {/* Timeline section */}
+                    <TimelineSection />
                 </div>
-
             </div>
+
+            {/* Dark blur effect */}
             <div className='fixed bottom-0 left-0 w-screen bg-black/90 blur-lg h-6'></div>
 
         </>
