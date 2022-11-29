60a5944d5fd4291b1afaf755ab9daeadb8441786
linus-jansson <contact@limpan.dev>
2022-11-29 21:55:08 +0100

    wrong url

---
+++ src/app/data.js
@@ -0,0 +1,45 @@
+export default {
+    projects: [
+        {
+            type: 'Full stack',
+            title: 'Adminnärvaro',
+            desc: 'A fulllstack web application for seeing the attendence of the personeel at NTI gymnasiet. Built with Next.js, JWT, Socket.io, Express.js, tailwindcss and Mongodb. The application also has an API that is used by a raspberry pico W to update the attendence for a person with a button press.',
+            page_link: 'https://narvaro.ntig.net/'
+        },
+        {
+            type: 'Full stack (ongoing)',
+            title: 'Website builder',
+            desc: 'A full stack application for creating websites which can be exported as a HTML file. Built with Next.js and Tailwindcss.',
+            github: 'https://github.com/linus-jansson/webpage-builder',
+            page_link: 'https://webpage-builder-inky.vercel.app/'
+        },
+        {
+            type: 'C# (ongoing)',
+            title: 'POS system',
+            desc: 'A POS system for hotels. Built with c# and winforms. Also uses NUnit Tests for unit testing.',
+            github: 'https://github.com/NTIG-Uppsala/hotell-kassasystem'
+        },
+        {
+            type: 'Frontend',
+            title: 'Pizzeria Rafiki',
+            desc: 'A website for a pizzeria. Has automated tests using Github workflows and selenium. Deployed using Github pages',
+            github: 'https://github.com/NTIG-Uppsala/pizzeria-rafiki/',
+            page_link: 'https://ntig-uppsala.github.io/pizzeria-rafiki/'
+        },
+        {
+            type: 'Frontend',
+            title: 'Frisör Saxé',
+            desc: 'A website for a hairdresser. Uses selenium for automated testing in combination with Github workflows.',
+            github: 'https://github.com/NTIG-Uppsala/Frisor-Saxe/',
+            page_link: 'https://ntig-uppsala.github.io/Frisor-Saxe/'
+        },
+        {
+            type: 'Frontend',
+            title: 'Florist Blåklinten',
+            desc: 'A website for a flowershop. Uses selenium SDK for unit tests and is deployed using Github Workflows on Github Pages',
+            github: 'https://github.com/NTIG-Uppsala/florist-blaklinten/',
+            page_link: 'https://ntig-uppsala.github.io/florist-blaklinten/'
+        },
+    ],
+    timeline: [{}]
+}
