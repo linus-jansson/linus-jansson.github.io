680417f45f55d79e8febde4e783f48671f7df4c7
linus-jansson <contact@limpan.dev>
2022-11-29 22:00:21 +0100

    fixed build error

--- src/components/index/Timeline.tsx
+++ src/components/index/Timeline.tsx
@@ -1,4 +1,4 @@
-import styles from '../../styles/index/Timeline.module.css'
+import styles from '../../styles/index/timeline.module.css'
 
 const TimelineCard = ({ title, children }: { title: string, children: React.ReactNode }) => {
     return (
@@ -15,7 +15,7 @@ const TimelineSection = () => {
     return (
         <>
             <div id='timeline' className='p-12'>
-                <h1 className='text-slate-100 text-3xl font-bold uppercase text-center tracking-widest'>My Timeline</h1>
+                <h1 className='text-3xl font-bold tracking-widest text-center uppercase text-slate-100'>My Timeline</h1>
                 <div>
                     <TimelineCard title="Teknikprogrammet (TE)">
                         <p>I went to NTI-gymnasiet, a high school which has its focus in tech from where I graduated in June 2022.</p>
@@ -24,7 +24,7 @@ const TimelineSection = () => {
                         <p>I am currently studying to become a <i>"Gymnasieingenjör"</i> at NTI-gymnasiet, an additional year to high school which prepares me to work in software development.</p>
                     </TimelineCard>
                 </div>
-                <p className='text-center text-zinc-600 font-thin'>Want to Hire me? <a href='mailto:contact@limpan.dev' className='underline'>Hit me up!</a></p>
+                <p className='font-thin text-center text-zinc-600'>Want to Hire me? <a href='mailto:contact@limpan.dev' className='underline'>Hit me up!</a></p>
             </div>
         </>
     )
