5798896acc934deb7210712d36a98023e06b45f1
linus-jansson <contact@limpan.dev>
2023-02-10 14:14:14 +0100

    updated data

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export default {
             id: 0,
             type: 'Full stack',
             title: 'Adminnärvaro',
-            desc: 'A full stack web application for seeing the attendence of the personeel at NTI gymnasiet. Built with Next.js, JWT, Socket.io, Express.js, tailwindcss and Mongodb. The application also has an API that is used by a raspberry pico W to update the attendence for a person with a button press.',
+            desc: 'A full stack web application for seeing the attendence of the personeel at NTI gymnasiet. Built with Next.js, JWT, Socket.io, Express.js, tailwindcss and Mongodb. The application also has an built in API that being used by a raspberry pico W to update the attendence for a person with a button press.',
             github: 'https://github.com/NTIG-Uppsala/admin-narvaro',
             page_link: 'https://narvaro.ntig.net/'
         },
@@ -24,14 +24,6 @@ export default {
         },
         {
             id: 3,
-            type: 'Full stack (ongoing)',
-            title: 'Website builder',
-            desc: 'A full stack application for creating websites which can be exported as a HTML file. Built with Next.js and Tailwindcss.',
-            github: 'https://github.com/linus-jansson/webpage-builder',
-            page_link: 'https://webpage-builder-inky.vercel.app/'
-        },
-        {
-            id: 4,
             type: 'Frontend',
             title: 'Florist Blåklinten',
             desc: 'A website for a flowershop. Uses selenium SDK for unit tests and is deployed using Github Workflows on Github Pages',
