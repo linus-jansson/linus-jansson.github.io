66a78e68371e73ff48ec9109409ca12ebec454e6
Jansson Linus (Systemutvecklare) <contact@limpan.dev>
2023-05-02 14:43:53 +0200

    needed to update portfolio deadline thing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,12 @@ export default function IndexPage() {
     return (
         <>
             {/* Mobile Navbar */}
-            <div className='md:hidden z-50 sticky shadow-2xl w-screen top-0 bg-rose-700 p-4 flex flex-row justify-around xs:flex-col'>
-                <a href="#start" className='text-slate-100 hover:text-slate-300 duration-100 font-bold uppercase tracking-widest hover:underline'>About Me</a>
-                <a href="#projects" className='text-slate-100 hover:text-slate-400 duration-100 font-bold uppercase tracking-widest hover:underline'>Projects</a>
-                <a href="#timeline" className='text-slate-100 hover:text-slate-400 duration-100 font-bold uppercase tracking-widest hover:underline'>Timeline</a>
+            <div className='sticky top-0 z-50 flex flex-row justify-around w-screen p-4 shadow-2xl md:hidden bg-rose-700 xs:flex-col'>
+                <a href="#start" className='font-bold tracking-widest uppercase duration-100 text-slate-100 hover:text-slate-300 hover:underline'>About Me</a>
+                <a href="#projects" className='font-bold tracking-widest uppercase duration-100 text-slate-100 hover:text-slate-400 hover:underline'>Projects</a>
+                <a href="#timeline" className='font-bold tracking-widest uppercase duration-100 text-slate-100 hover:text-slate-400 hover:underline'>Timeline</a>
             </div>
-            <div id='start' className='scroll-mt-12 flex flex-col lg:flex-row pt-4'>
+            <div id='start' className='flex flex-col pt-4 scroll-mt-12 lg:flex-row'>
                 {/* Sidebar with links and about text */}
                 <Header />
                 <div className='px-4 md:ml-auto md:pr-24 md:pl-48 md:pt-24 mb-12 w-full md:w-1/2 flex flex-col max-w-[1024px] justify-center'>
@@ -24,7 +24,7 @@ export default function IndexPage() {
             </div>
 
             {/* Dark blur effect */}
-            <div className='fixed bottom-0 left-0 w-screen bg-black/90 blur-lg h-6'></div>
+            <div className='fixed bottom-0 left-0 w-screen h-6 bg-black/90 blur-lg'></div>
 
         </>
     )
--- src/components/index/Timeline.tsx
+++ src/components/index/Timeline.tsx
@@ -17,11 +17,14 @@ const TimelineSection = () => {
             <div id='timeline' className='p-12'>
                 <h1 className='text-3xl font-bold tracking-widest text-center uppercase text-slate-100'>My Timeline</h1>
                 <div>
-                    <TimelineCard title="Teknikprogrammet (TE)">
-                        <p>I went to NTI-gymnasiet, a high school which has its focus in tech from where I graduated in June 2022.</p>
+                    <TimelineCard title="Software developer at Uppsala Komnun">
+                        <p>After my internship at Uppsala Kommun I got a fulltime job as a software developer.</p>
                     </TimelineCard>
                     <TimelineCard title="Gymnasieingenjör (T4)">
                         <p>I am currently studying to become a <i>"Gymnasieingenjör"</i> at NTI-gymnasiet, an additional year to high school which prepares me to work in software development.</p>
+                    </TimelineCard>
+                    <TimelineCard title="Teknikprogrammet (TE)">
+                        <p>I went to NTI-gymnasiet, a high school which has its focus in tech from where I graduated in June 2022.</p>
                     </TimelineCard>
                 </div>
                 <p className='font-thin text-center text-zinc-600'>Want to Hire me? <a href='mailto:contact@limpan.dev' className='underline'>Hit me up!</a></p>
