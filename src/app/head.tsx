9ebde203bf7e75d6dd81987cfa16b72e819b8a9f
Linus J <30862662+linus-jansson@users.noreply.github.com>
2023-10-29 00:55:18 +0200

    meta tag fixses

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,28 +2,6 @@ export default async function Head() {
 
     return (
         <>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-            {/* Open graphs tags */}
-            <meta name="title" content="Hey! I'm Linus!" />
-            <meta name="description" content="Full stack developer from Sweden. Here is my portfolio!" />
-
-            <meta property="og:type" content="website" />
-            <meta property="og:title" content="Hey! I'm Linus!" />
-            <meta property="og:description" content="Full stack developer from Sweden. Here is my portfolio!" />
-            <meta property="og:url" content="https://limpan.dev/" />
-            <meta property="og:image" content="https://limpan.dev/images/background.jpg" />
-
-            <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:title" content="Hey! I'm Linus!" />
-            <meta name="twitter:description" content="Full stack developer from Sweden. Here is my portfolio!" />
-            <meta name="twitter:image" content="https://limpan.dev/images/background.jpg" />
-
-            {/* IOS Theme */}
-            <meta name="theme-color" content="#18181B"/>
-            <meta name="apple-mobile-web-app-capable" content="yes"/>
-            <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-
             <title>Hey! I&apos;m Linus!</title>
         </>
     )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,22 @@
+import { Metadata } from 'next';
 import './globals.css'
+
+export const metadata: Metadata = {
+    title: 'Hey! I\'m Linus!',
+    description: 'Full stack developer from Sweden. Here is my portfolio!',
+    openGraph: {
+        type: 'website',
+        title: 'Hey! I\'m Linus!',
+        description: 'Full stack developer from Sweden. Here is my portfolio!',
+        url: 'https://limpan.dev/',
+        images: 'https://limpan.dev/images/background.jpg',
+    },
+    other: {
+        'theme-color': '#18181B',
+        'apple-mobile-web-app-capable': 'true',
+        'apple-mobile-web-app-status-bar-style': 'black-translucent',
+    }
+}
 export default function RootLayout({
     children,
 }: {
